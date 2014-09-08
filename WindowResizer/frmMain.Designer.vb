370f0c846ca8c1827ec994fb9ee03a1a4eec191c
byronlcollier <byronlcollier@gmail.com>
2014-09-08 20:01:06 +0100

    Initial Commit

---
+++ WindowResizer/frmMain.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWindowResizer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cbRefresh = New System.Windows.Forms.Button()
+        Me.cbDropDown = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.cboPreSetResolutions = New System.Windows.Forms.ComboBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtWidth = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnResize = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cbRefresh)
+        Me.GroupBox1.Controls.Add(Me.cbDropDown)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(491, 82)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "List of Processes"
+        '
+        'cbRefresh
+        '
+        Me.cbRefresh.Location = New System.Drawing.Point(7, 48)
+        Me.cbRefresh.Name = "cbRefresh"
+        Me.cbRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.cbRefresh.TabIndex = 1
+        Me.cbRefresh.Text = "Refresh"
+        Me.cbRefresh.UseVisualStyleBackColor = True
+        '
+        'cbDropDown
+        '
+        Me.cbDropDown.FormattingEnabled = True
+        Me.cbDropDown.Location = New System.Drawing.Point(7, 20)
+        Me.cbDropDown.Name = "cbDropDown"
+        Me.cbDropDown.Size = New System.Drawing.Size(478, 21)
+        Me.cbDropDown.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.cboPreSetResolutions)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 102)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(491, 60)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Pre-Set Resolutions"
+        '
+        'cboPreSetResolutions
+        '
+        Me.cboPreSetResolutions.FormattingEnabled = True
+        Me.cboPreSetResolutions.Location = New System.Drawing.Point(6, 19)
+        Me.cboPreSetResolutions.Name = "cboPreSetResolutions"
+        Me.cboPreSetResolutions.Size = New System.Drawing.Size(478, 21)
+        Me.cboPreSetResolutions.TabIndex = 1
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.txtHeight)
+        Me.GroupBox3.Controls.Add(Me.txtWidth)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Location = New System.Drawing.Point(13, 169)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(491, 81)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Custom Resolutions"
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(91, 46)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(100, 20)
+        Me.txtHeight.TabIndex = 3
+        '
+        'txtWidth
+        '
+        Me.txtWidth.Location = New System.Drawing.Point(91, 20)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(100, 20)
+        Me.txtWidth.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Window Height"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Window Width"
+        '
+        'btnResize
+        '
+        Me.btnResize.Location = New System.Drawing.Point(13, 257)
+        Me.btnResize.Name = "btnResize"
+        Me.btnResize.Size = New System.Drawing.Size(75, 23)
+        Me.btnResize.TabIndex = 3
+        Me.btnResize.Text = "Resize"
+        Me.btnResize.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(94, 257)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmWindowResizer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(517, 296)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnResize)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmWindowResizer"
+        Me.Text = "Window Resizer"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cbRefresh As System.Windows.Forms.Button
+    Friend WithEvents cbDropDown As System.Windows.Forms.ComboBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents cboPreSetResolutions As System.Windows.Forms.ComboBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtHeight As System.Windows.Forms.TextBox
+    Friend WithEvents txtWidth As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnResize As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ WindowResizer/frmMain.vb
@@ -0,0 +1,154 @@
+﻿Imports System.Runtime.InteropServices
+Public Class frmWindowResizer
+    Dim lstResolutionList As New List(Of Resolution)
+    <DllImport("user32.dll", EntryPoint:="MoveWindow", CharSet:=CharSet.Auto)> _
+    Public Shared Function MoveWindow(ByVal hWnd As IntPtr, _
+        ByVal X As Int32, _
+        ByVal Y As Int32, _
+        ByVal nWidth As Int32, _
+        ByVal nHeight As Int32, _
+        ByVal bRepaint As Boolean _
+        ) As Boolean
+    End Function
+    Private Sub btnResize_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnResize.Click
+        Dim oItem As runningProcess = CType(cbDropDown.SelectedItem, runningProcess)
+        Dim intX As Integer
+        Dim intY As Integer
+        If Not cbDropDown.SelectedItem Is Nothing Then
+            intX = Convert.ToInt32(txtWidth.Text)
+            intY = Convert.ToInt32(txtHeight.Text)
+            MoveWindow(oItem.Value, 10, 10, Convert.ToInt32(txtWidth.Text), Convert.ToInt32(txtHeight.Text), True) 'Resize
+        End If
+    End Sub
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+        ' Add any initialization after the InitializeComponent() call.
+        Call Initialise()
+    End Sub
+    Private Sub Initialise()
+        Call GetProcessList()
+        Call InitialiseResolutionList()
+        Call LoadResolutionComboBox()
+    End Sub
+    Private Sub InitialiseResolutionList()
+        lstResolutionList.Add(New Resolution("SVGA", "4:3", 800, 600))
+        lstResolutionList.Add(New Resolution("WSVGA", "~17:10", 1024, 600))
+        lstResolutionList.Add(New Resolution("XGA", "4:3", 1024, 768))
+        lstResolutionList.Add(New Resolution("XGA+", "4:3", 1152, 864))
+        lstResolutionList.Add(New Resolution("WXGA", "16:9", 1280, 720))
+        lstResolutionList.Add(New Resolution("WXGA", "5:3", 1280, 768))
+        lstResolutionList.Add(New Resolution("WXGA", "16:10", 1280, 800))
+        lstResolutionList.Add(New Resolution("SXGA– (UVGA)", "4:3", 1280, 960))
+        lstResolutionList.Add(New Resolution("SXGA", "5:4", 1280, 1024))
+        lstResolutionList.Add(New Resolution("HD", "~16:9", 1360, 768))
+        lstResolutionList.Add(New Resolution("HD", "~16:9", 1366, 768))
+        lstResolutionList.Add(New Resolution("SXGA+", "4:3", 1400, 1050))
+        lstResolutionList.Add(New Resolution("WXGA+", "16:10", 1440, 900))
+        lstResolutionList.Add(New Resolution("HD+", "16:9", 1600, 900))
+        lstResolutionList.Add(New Resolution("UXGA", "4:3", 1600, 1200))
+        lstResolutionList.Add(New Resolution("WSXGA+", "16:10", 1680, 1050))
+        lstResolutionList.Add(New Resolution("FHD", "16:9", 1920, 1080))
+        lstResolutionList.Add(New Resolution("WUXGA", "16:10", 1920, 1200))
+        lstResolutionList.Add(New Resolution("QWXGA", "16:9", 2048, 1152))
+        lstResolutionList.Add(New Resolution("WQHD", "16:9", 2560, 1440))
+        lstResolutionList.Add(New Resolution("WQXGA", "16:10", 2560, 1600))
+    End Sub
+    Private Sub LoadResolutionComboBox()
+        For Each res As Resolution In lstResolutionList
+            Me.cboPreSetResolutions.Items.Add(res.Name & " (" & res.AspectRatio & ")")
+        Next
+    End Sub
+    Private Sub cbRefresh_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbRefresh.Click
+        Call GetProcessList()
+    End Sub
+    Public Sub GetProcessList()
+        Dim procs() As Process = Process.GetProcesses()
+        Dim proc As Process
+        Dim hWnd As IntPtr
+        For Each proc In procs
+            hWnd = proc.MainWindowHandle
+            If Not (hWnd = IntPtr.Zero) Then
+                cbDropDown.Items.Add(New runningProcess(proc.MainWindowTitle, proc.MainWindowHandle))
+            End If
+        Next proc
+    End Sub
+    Private Sub cboPreSetResolutions_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboPreSetResolutions.SelectedIndexChanged
+        Dim i As Integer
+        i = cboPreSetResolutions.SelectedIndex
+        txtWidth.Text = lstResolutionList(i).Width
+        txtHeight.Text = lstResolutionList(i).Height
+    End Sub
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+End Class
+Public Class runningProcess
+    Private mText As String
+    Private mValue As String
+
+    Public Sub New(ByVal pText As String, ByVal pValue As String)
+        mText = pText
+        mValue = pValue
+    End Sub
+
+    Public ReadOnly Property Text() As String
+        Get
+            Return mText
+        End Get
+    End Property
+
+    Public ReadOnly Property Value() As String
+        Get
+            Return mValue
+        End Get
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return mText
+    End Function
+End Class
+Public Class Resolution
+    Private strAbbreviation As String
+    Private strAspectRatio As String
+    Private intWidth As Integer
+    Private intHeight As Integer
+    Public Sub New(ByVal Name As String, ByVal AspectRatio As String, ByVal Width As Integer, ByVal Height As Integer)
+        strAbbreviation = Name
+        strAspectRatio = AspectRatio
+        intWidth = Width
+        intHeight = Height
+    End Sub
+    Public Property Name As String
+        Get
+            Return strAbbreviation
+        End Get
+        Set(ByVal value As String)
+            strAbbreviation = value
+        End Set
+    End Property
+    Public Property AspectRatio As String
+        Get
+            Return strAspectRatio
+        End Get
+        Set(ByVal value As String)
+            strAspectRatio = value
+        End Set
+    End Property
+    Public Property Width As Integer
+        Get
+            Return intWidth
+        End Get
+        Set(ByVal value As Integer)
+            intWidth = value
+        End Set
+    End Property
+    Public Property Height As Integer
+        Get
+            Return intHeight
+        End Get
+        Set(ByVal value As Integer)
+            intHeight = value
+        End Set
+    End Property
+End Class
